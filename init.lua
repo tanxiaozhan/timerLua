6df01f21008159ea9327b6fc627c859aeb4f29be
txz <txz@163.com>
2018-01-16 22:54:46 +0800

    update init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
+--read alarm form file
+local alarmON={0,8,0,0,1,0,9,0,0,1}
+local alarmOFF={}
+local af=file.open("Alarm.Dat","r")
+if af then
+    
+
+end
+
 dofile("remote.lua")
 dofile("httpServer.lua")
